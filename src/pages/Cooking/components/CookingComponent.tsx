fbbe6efcda2b4f49a9dac3fb18812d360e72b296
Andrija Eremic <andrija.eremic@gmail.com>
2023-12-26 20:39:53 +0100

    carousel

--- src/pages/Cooking/components/CookingComponent.tsx
+++ src/pages/Cooking/components/CookingComponent.tsx
@@ -1,5 +1,37 @@
 const CookingComponent = () => {
-  return <>cooking</>;
+  return (
+    <div className="container min-h-screen w-auto sm:w-1/2">
+      <div className="grid grid-cols-1 gap-6">
+        <div className="card sm:card-side bg-base-100 shadow-xl">
+          <div className="card-body container mx-auto flex items-center sm:max-w-md">
+            <div className="w-64 carousel rounded-box">
+              <div className="carousel-item w-full">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+                  className="w-full"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+              <div className="carousel-item w-full">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+                  className="w-full"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+              <div className="carousel-item w-full ">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+                  className="w-full"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CookingComponent;
