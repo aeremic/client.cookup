809fe695db3fd640b09e7399c54b454e6ce5649c
Andrija Eremic <andrija.eremic@gmail.com>
2024-05-19 18:50:10 +0200

    resource file change

--- src/pages/Cooking/components/CommentsComponent.tsx
+++ src/pages/Cooking/components/CommentsComponent.tsx
@@ -98,159 +98,167 @@ const CommentsComponent = () => {
   };
 
   return (
-    <div className="w-auto sm:w-1/2 container">
-      <div className="prose lg:prose-lg mb-3 text-center sm:text-left">
-        <h3>{t("Comments")}</h3>
-      </div>
-      {commentsLoaded && comments.length > 0 ? (
-        <div>
-          <div className="gap-6 grid grid-cols-1">
-            <div className="bg-base-100 shadow-xl card">
-              <div className="mx-auto card-body container">
-                <div>
-                  {comments.map((comment, index) => (
-                    <div key={index}>
-                      <div className="flex flex-row sm:mb-3 text-left">
-                        <div className="mb-3 sm:mb-0 sm:basis-1/6 avatar placeholder">
-                          <div className="bg-primary rounded-xl w-12 sm:w-16 text-neutral-content">
-                            <span className="text-xl">
-                              {comment.user?.username
-                                ? comment.user?.username[0]?.toUpperCase()
-                                : ""}
-                            </span>
+    <div>
+      <div className="w-auto sm:w-1/2 container">
+        <div className="prose lg:prose-lg mb-3 text-center sm:text-left">
+          <h3>{t("Comments")}</h3>
+        </div>
+        {commentsLoaded && comments.length > 0 ? (
+          <div>
+            <div className="gap-6 grid grid-cols-1">
+              <div className="bg-base-100 shadow-xl card">
+                <div className="mx-auto card-body container">
+                  <div>
+                    {comments.map((comment, index) => (
+                      <div key={index}>
+                        <div className="flex flex-row sm:mb-3 text-left">
+                          <div className="mb-3 sm:mb-0 sm:basis-1/6 avatar placeholder">
+                            <div className="bg-primary rounded-xl w-12 sm:w-16 text-neutral-content">
+                              <span className="text-xl">
+                                {comment.user?.username
+                                  ? comment.user?.username[0]?.toUpperCase()
+                                  : ""}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="ml-5 sm:ml-0">
+                            <div className="prose lg:prose-lg">
+                              <h4>{comment.user.username}</h4>
+                            </div>
+                            <div className="sm:mt-1 rating rating-sm sm:rating-md">
+                              <input
+                                type="radio"
+                                name="rating-5"
+                                className="bg-red-400 mask mask-heart"
+                                disabled={true}
+                              />
+                              <input
+                                type="radio"
+                                name="rating-5"
+                                className="bg-red-400 mask mask-heart"
+                                disabled={true}
+                                checked
+                              />
+                              <input
+                                type="radio"
+                                name="rating-5"
+                                className="bg-red-400 mask mask-heart"
+                                disabled={true}
+                              />
+                              <input
+                                type="radio"
+                                name="rating-5"
+                                className="bg-red-400 mask mask-heart"
+                                disabled={true}
+                              />
+                              <input
+                                type="radio"
+                                name="rating-5"
+                                className="bg-red-400 mask mask-heart"
+                                disabled={true}
+                              />
+                            </div>
                           </div>
                         </div>
-                        <div className="ml-5 sm:ml-0">
-                          <div className="prose lg:prose-lg">
-                            <h4>{comment.user.username}</h4>
+                        {comment.content.length > 0 ? (
+                          <div className="bg-base-200 card">
+                            <div className="mx-auto card-body container">
+                              <div className="prose lg:prose-base">
+                                <p>
+                                  <i>{comment.content}</i>
+                                </p>
+                              </div>
+                            </div>
                           </div>
-                          <div className="sm:mt-1 rating rating-sm sm:rating-md">
+                        ) : (
+                          <></>
+                        )}
+                        <div className="divider"></div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="bg-base-100 shadow-xl mt-4 card">
+          <div className="mx-auto card-body container">
+            <div>
+              <div className="flex flex-row sm:mb-3 text-left"></div>
+              <div className="bg-base-200 card">
+                <div className="mx-auto text-center card-body container">
+                  <div>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <div className="prose lg:prose-base">
+                          <span className="label-text">
+                            {t("LeaveYourComment")}
+                          </span>
+                        </div>
+                      </div>
+                      <form onSubmit={handleSubmit}>
+                        <textarea
+                          className="textarea-bordered w-full textarea"
+                          placeholder="Comment"
+                          onChange={handleNewCommentChange}
+                          value={newCommentContent}
+                        ></textarea>
+                        <div className="mt-4 sm:mt-4 sm:text-left">
+                          <div className="sm:mt-1 rating rating-md sm:rating-md">
                             <input
                               type="radio"
                               name="rating-5"
                               className="bg-red-400 mask mask-heart"
-                              disabled={true}
+                              disabled={false}
                             />
                             <input
                               type="radio"
                               name="rating-5"
                               className="bg-red-400 mask mask-heart"
-                              disabled={true}
+                              disabled={false}
                               checked
                             />
                             <input
                               type="radio"
                               name="rating-5"
                               className="bg-red-400 mask mask-heart"
-                              disabled={true}
+                              disabled={false}
                             />
                             <input
                               type="radio"
                               name="rating-5"
                               className="bg-red-400 mask mask-heart"
-                              disabled={true}
+                              disabled={false}
                             />
                             <input
                               type="radio"
                               name="rating-5"
                               className="bg-red-400 mask mask-heart"
-                              disabled={true}
+                              disabled={false}
                             />
                           </div>
                         </div>
-                      </div>
-                      <div className="bg-base-200 card">
-                        <div className="mx-auto card-body container">
-                          <div className="prose lg:prose-base">
-                            <p>
-                              <i>{comment.content}</i>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="divider"></div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="bg-base-100 shadow-xl mt-4 card">
-        <div className="mx-auto card-body container">
-          <div>
-            <div className="flex flex-row sm:mb-3 text-left"></div>
-            <div className="bg-base-200 card">
-              <div className="mx-auto text-center card-body container">
-                <div>
-                  <label className="form-control w-full">
-                    <div className="label">
-                      <div className="prose lg:prose-base">
-                        <span className="label-text">Leave your comment</span>
-                      </div>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                      <textarea
-                        className="textarea-bordered w-full textarea"
-                        placeholder="Comment"
-                        onChange={handleNewCommentChange}
-                        value={newCommentContent}
-                      ></textarea>
-                      <div className="mt-4 sm:mt-4 sm:text-left">
-                        <div className="sm:mt-1 rating rating-md sm:rating-md">
-                          <input
-                            type="radio"
-                            name="rating-5"
-                            className="bg-red-400 mask mask-heart"
-                            disabled={false}
-                          />
-                          <input
-                            type="radio"
-                            name="rating-5"
-                            className="bg-red-400 mask mask-heart"
-                            disabled={false}
-                            checked
-                          />
-                          <input
-                            type="radio"
-                            name="rating-5"
-                            className="bg-red-400 mask mask-heart"
-                            disabled={false}
-                          />
-                          <input
-                            type="radio"
-                            name="rating-5"
-                            className="bg-red-400 mask mask-heart"
-                            disabled={false}
-                          />
-                          <input
-                            type="radio"
-                            name="rating-5"
-                            className="bg-red-400 mask mask-heart"
-                            disabled={false}
-                          />
-                        </div>
-                      </div>
-                      <button
-                        className="shadow-xl mt-4 btn btn-md btn-primary"
-                        type="submit"
-                      >
-                        <svg
-                          className="w-6"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 24 24"
+                        <button
+                          className="shadow-xl mt-4 btn btn-md btn-primary"
+                          type="submit"
                         >
-                          <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
-                        </svg>
-                        {t("PostComment")}
-                      </button>
-                    </form>
-                  </label>
+                          <svg
+                            className="w-6"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+                          </svg>
+                          {t("PostComment")}
+                        </button>
+                      </form>
+                    </label>
+                  </div>
                 </div>
               </div>
             </div>
@@ -260,7 +268,7 @@ const CommentsComponent = () => {
       {showAlertSuccess ? (
         <div className="toast">
           <div className="alert alert-success">
-            <span>Comment successfully added.</span>
+            <span>{t("CommentSuccessfullyAdded")}</span>
           </div>
         </div>
       ) : (
@@ -269,7 +277,7 @@ const CommentsComponent = () => {
       {showAlertFailure ? (
         <div className="toast">
           <div className="alert alert-error">
-            <span>Something went wrong.</span>
+            <span>{t("SomethingWentWrong")}</span>
           </div>
         </div>
       ) : (
